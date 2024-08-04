0ff6d7f907889698f5ba4702274c0a01ce50ebd2
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-08-04 12:22:09 +0300

    shopping list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// src/App.jsx
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
 
 const App = () => {
+  const items = [
+    { name: 'Milk', purchased: true },
+    { name: 'Bread', purchased: false },
+    { name: 'Eggs', purchased: true},
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Shopping List</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} className={item.purchased ? 'completed' : 'not-completed'}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
